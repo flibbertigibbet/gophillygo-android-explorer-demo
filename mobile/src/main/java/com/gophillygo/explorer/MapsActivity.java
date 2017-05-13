5070969a9bd678c934d8cb3e1da558689e8c74bc
Kathryn Killebrew <kkillebrew@azavea.com>
2017-05-13 14:19:55 -0400

    Read data from firebase
    Read in destinations from Firebase on activity load.

--- mobile/src/main/java/com/gophillygo/explorer/MapsActivity.java
+++ mobile/src/main/java/com/gophillygo/explorer/MapsActivity.java
@@ -2,6 +2,8 @@ package com.gophillygo.explorer;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +11,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.gophillygo.explorer.models.Destination;
+
+import java.util.HashMap;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -22,6 +33,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(com.gophillygo.explorer.R.id.map);
         mapFragment.getMapAsync(this);
+
+        fetchFirebase();
+    }
+
+    // TODO: move this to a service
+    private void fetchFirebase() {
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener firebaseListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("Firebase", "onDataChange !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                Log.d("Firebase", dataSnapshot.toString());
+                Log.d("Firebase", "Child count: " + dataSnapshot.getChildrenCount());
+
+                SparseArray<Destination> destinations = new SparseArray<>((int)dataSnapshot.getChildrenCount());
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    Destination destination = snapshot.getValue(Destination.class);
+                    int id = Integer.valueOf(snapshot.getKey());
+                    destination.setId(id);
+                    destinations.append(id, destination);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Firebase", "onCancelled");
+            }
+        };
+
+        database.addListenerForSingleValueEvent(firebaseListener);
+
     }
 
 
