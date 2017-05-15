0d88b368fe73f79027e55b5b2189acaee9c81276
Kathryn Killebrew <kkillebrew@azavea.com>
2017-05-15 11:31:54 -0400

    Wrap place detail in scroll view
    So image and header will scroll as well as content.

--- mobile/src/main/java/com/gophillygo/explorer/MapsActivity.java
+++ mobile/src/main/java/com/gophillygo/explorer/MapsActivity.java
@@ -2,7 +2,6 @@ package com.gophillygo.explorer;
 
 import android.net.Uri;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
